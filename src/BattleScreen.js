b5a5359755bdb63f93cdd2e8fff4109062bbe0c7
twtk1 <twtk1@unigroupinc.com>
2018-09-17 09:07:42 -0500

    added play again to lose screen

--- src/BattleScreen.js
+++ src/BattleScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import World from './features/world'
+import Store from './config/store'
 
 class BattleScreen extends Component {
+  purge(){
+    Store.persistor.purge(),
+    window.location = "/"
+  }
   render() {
-    return (
+    return (<div>
       <div>
         <img src="https://78.media.tumblr.com/59275b5a8ad93aef23be16fb506a77ea/tumblr_nvka7e6AmH1uxpv1ro1_r1_400.gif"/>
+      </div>
+      <button onClick={this.purge}>Play Again</button>
       </div>
     )
   }
